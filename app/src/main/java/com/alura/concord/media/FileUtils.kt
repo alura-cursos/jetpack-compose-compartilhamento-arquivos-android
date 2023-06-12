114f77ea891052c32637ef9625d4a76f24c9f29f
Junior <35709152+git-jr@users.noreply.github.com>
2023-06-12 15:25:52 -0300

    aula 2.1

--- app/src/main/java/com/alura/concord/media/FileUtils.kt
+++ app/src/main/java/com/alura/concord/media/FileUtils.kt
@@ -1,5 +1,12 @@
 package com.alura.concord.media
 
+import android.content.Context
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.withContext
+import okio.use
+import java.io.File
+import java.io.InputStream
+
 fun Long.formatReadableFileSize(): String {
     val size = this
     val kilobyte = 1024
@@ -11,5 +18,25 @@ fun Long.formatReadableFileSize(): String {
         size < megaByte -> "${size / kilobyte} KB"
         size < gigaByte -> "${size / megaByte} MB"
         else -> "${size / gigaByte} GB"
+    }
+}
+
+
+suspend fun Context.saveOnInternalStorage(
+    inputStream: InputStream,
+    fileName: String,
+    onSuccess: (String) -> Unit
+) {
+    val path = getExternalFilesDir("temp")
+    val newFile = File(path, fileName)
+
+    withContext(IO) {
+        newFile.outputStream().use { file ->
+            inputStream.copyTo(file)
+        }
+
+        if (newFile.exists()) {
+            onSuccess(newFile.path)
+        }
     }
 }
--- app/src/main/java/com/alura/concord/network/DownloadService.kt
+++ app/src/main/java/com/alura/concord/network/DownloadService.kt
@@ -1,29 +1,25 @@
 package com.alura.concord.network
 
-import android.content.Context
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okio.use
-import java.io.File
 import java.io.InputStream
 
 object DownloadService {
 
-    suspend fun makeDownloadByURL(url: String, context: Context) {
+    suspend fun makeDownloadByURL(
+        url: String,
+        onFinishedDownload: (InputStream) -> Unit
+    ) {
         val client = OkHttpClient()
         val request = Request.Builder().url(url).build()
 
         withContext(IO) {
             client.newCall(request).execute().use { response ->
                 response.body?.byteStream()?.use { fileData: InputStream ->
-                    val path = context.getExternalFilesDir("temp")
-                    val newFile = File(path, "Teste.png")
-
-                    newFile.outputStream().use { file ->
-                        fileData.copyTo(file)
-                    }
+                    onFinishedDownload(fileData)
                 }
             }
         }
